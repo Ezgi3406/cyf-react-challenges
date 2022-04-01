e53973bdaf4f16e7f1ceaf09416945e8f765a1b3
ezgi <ezgigunes3406@gmail.com>
2022-04-01 20:59:58 +0100

    Lesson2 completed

---
+++ challenge-high-score-tables/src/App.js
@@ -0,0 +1,25 @@
+// import logo from "./logo.svg";
+import "./App.css";
+// import HighScoreTable from "./HighScoreTable";
+import AllTables from "./HighScoreTable";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <AllTables />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ challenge-high-score-tables/src/HighScoreTable.js
@@ -0,0 +1,73 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable no-trailing-spaces */
+import React from "react";
+import allCountryScores from "./scores";
+
+//table with country name as heading
+//list of players and scores
+
+//PlayerScore = tr elem
+//<PlayerScore/>
+
+//Need to create highscoretable for reach elem in Scores arr
+// allCountryScores.map(country => {return all of country-div contents})
+
+console.log(allCountryScores); //logs arr of 8 obj elems
+
+const AllTables = () => {
+  // function to sort allCountryScores arr alphabetically using name property value
+  function compare(a, b) {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  }
+  const sortedCountriesArr = allCountryScores.sort(compare);
+
+  return (
+    <div className="wrapper">
+      <h2>High Scores per Country</h2>
+      {sortedCountriesArr.map((country, index) => (
+        <HighScoreTable country={country} key={index} />
+      ))}
+    </div>
+  );
+};
+
+//map through scores arr
+//for each row renders name & score
+//pass name & score as 2 different props in PlayerScore component
+//Use props in playerscore to rnder row
+const HighScoreTable = ({ country }) => {
+  return (
+    <div className="country-div">
+      <h3>High Scores: {country.name}</h3>
+      <table>
+        <tbody>
+          {/*Scores arr sorted in descending order of s property values, then mapped to render PlayerScore row for each player. Name & score props created to access n & s values  */}
+          {country.scores
+            .sort((a, b) => b.s - a.s)
+            .map((score, index) => (
+              <PlayerScore name={score.n} score={score.s} key={index} />
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+//name & score props passed, both values will render in own td (1 tr)
+const PlayerScore = ({ name, score }) => {
+  return (
+    <tr>
+      <th scope="row"></th>
+      <td>{name}</td>
+      <td>{score}</td>
+    </tr>
+  );
+};
+
+export default AllTables;
